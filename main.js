56a405fea882cec61cfd80e377f80d0fd8a86b08
Harold Espinoza <haroldesptru@gmail.com>
2020-05-12 23:23:42 -0600

    Añadido el salto de las canciones en modo playlist del tocadiscos. Se añadio la funcionabilidad del mostrar el tiempo transcurrido durante cada canción

--- main.js
+++ main.js
@@ -8,6 +8,22 @@ const disk = document.getElementById("disk"),
     currentTimeIndicator = document.getElementById("current-time"),
     lastTimeIndicator = document.getElementById("last-time");
 
+// Variables de utilidad
+let currentSong = { index: -1, path: '' },
+    isStarted = false,
+    isLoaded= false,
+    currentTime = 0,
+    currentTimeInterval = null;
+
+// Arreglo con la lista de las canciones
+const SONGS = new Array(
+    {path:"./canciones/011.mp3",artist:"Nacho",song:"Bailame"},
+    {path:"./canciones/012.mp3",artist:"Chino y Nacho ft. Daddy Yankee",song:"Andas en mi cabeza"},
+    {path:"./canciones/013.mp3",artist:"Danny Ocean",song:"Me rehuso"},
+    {path:"./canciones/014.mp3",artist:"Danny Ocean",song:"Dembow"},
+    {path:"./canciones/015.mp3",artist:"Danny Ocean",song:"Vuelve"}
+ );
+
 // Objetos de configuración para los estilos de los discos y las agujas
 const diskStyle = {
         get Disk1(){ return "disk1"; },
@@ -22,21 +38,15 @@ const diskStyle = {
 
 const turntableConfig = {
     disk: diskStyle.Disk2,
-    arm: armStyle.Arm3
+    arm: armStyle.Arm3,
+    playerType: "playlist", // Tipo de reproductor, dos opciones: 'playlist' o 'stream'
+    source: SONGS // Recurso a reproducir, lista de canciones para la opción de 'playlist' o la url para la opción de 'stream'
 };
 
 // Declaraciones para el audio
 let player = new Audio(),
     shuffleSongs = new Array();
 
-// Arreglo con la lista de las canciones
-const SONGS = new Array(
-       {ruta:"./canciones/011.mp3",artista:"Nacho",cancion:"Bailame"},
-       {ruta:"./canciones/012.mp3",artista:"Chino y Nacho ft. Daddy Yankee",cancion:"Andas en mi cabeza"},
-       {ruta:"./canciones/013.mp3",artista:"Danny Ocean",cancion:"Me rehuso"},
-       {ruta:"./canciones/014.mp3",artista:"Danny Ocean",cancion:"Dembow"},
-       {ruta:"./canciones/015.mp3",artista:"Danny Ocean",cancion:"Vuelve"}
-    );
 
 // Prototipo de la función shuffle para crear el arreglo aleatorio de las canciones
 Array.prototype.shuffle = function() {
@@ -57,11 +67,11 @@ Array.prototype.shuffle = function() {
 
 // Acortador para 'addEventListener'
 window.Node.prototype.on = function(eventType, callback){
-  if(eventType === undefined || typeof eventType !== "string") return;
-  if(callback === undefined || typeof callback !== "function") return;
+    if(eventType === undefined || typeof eventType !== "string") return;
+    if(callback === undefined || typeof callback !== "function") return;
 
-  this.addEventListener(`${eventType}`, callback);
-  return this;
+    this.addEventListener(`${eventType}`, callback);
+    return this;
 };
 window.on = window.Node.prototype.on;
 
@@ -89,19 +99,67 @@ function getReadableTime(duration) {
     return tiempo;
 }
 
+function onCurrentTimeInterval() {
+    if(currentTime < player.duration - 1) {
+        currentTime++;
+        currentTimeIndicator.innerHTML = getReadableTime(currentTime);
+    }
+}
+
 function onLoadPlayerData() {
     lastTimeIndicator.innerHTML = getReadableTime(player.duration);
     arm.style.animationDuration = player.duration + "s";
+    isLoaded = true;
+}
+
+function onEndedPlayerData() {
+    onClickBtnStop();
+    
+    let playlistLength = turntableConfig.source.length,
+        currentSongIndex = currentSong.index;
+
+    if(turntableConfig.playerType === 'playlist') {
+        if(currentSongIndex < playlistLength - 1){
+            currentSong = {
+                index: (currentSongIndex + 1),
+                path: turntableConfig.source[currentSongIndex + 1].path
+            }
+
+            player.src = currentSong.path;
+            setTimeout(onClickBtnStart, 1000);
+        }
+    }
 }
 
 function onClickBtnStart(e) {
+    if(!isStarted){
+        if(turntableConfig.playerType === 'playlist'){
+            if(turntableConfig.source.length > 0){
+                currentSong = {
+                    index: 0,
+                    path: turntableConfig.source[0].path
+                };
+            }
+        }else if(turntableConfig.playerType === 'stream'){
+            currentSong = {
+                index: -1,
+                path: turntableConfig.source
+            }
+        }
+
+        player.src = currentSong.path;
+        isStarted = true;
+    }
+
     if(btnStart.classList.contains("to_pause")){
         player.pause();
+        clearInterval(currentTimeInterval);
         btnStart.classList.remove("to_pause");
         disk.style.animationPlayState = "paused";
         arm.style.animationPlayState = "paused";
     }else{
         player.play();
+        currentTimeInterval = setInterval(onCurrentTimeInterval, 1000);
         btnStart.classList.add("to_pause");
         disk.classList.add("active");
         arm.classList.add("active");
@@ -131,6 +189,10 @@ function onUpdateVolumeControl(e){
 }
 
 function onClickBtnStop(e) {
+    player.pause();
+    player.currentTime = 0;
+    clearInterval(currentTimeInterval);
+    currentTime = 0;
     btnStart.classList.remove("to_pause");
     disk.classList.remove("active");
     arm.classList.remove("active");
@@ -146,6 +208,7 @@ window.addEventListener("load", function () {
     })();
 
     player.onloadeddata = onLoadPlayerData;
+    player.onended = onEndedPlayerData;
 
     btnStart.on("click", onClickBtnStart);
 
@@ -154,6 +217,4 @@ window.addEventListener("load", function () {
     volumeIndicator.on("updatevolumecontrol", onUpdateVolumeControl);
 
     btnStop.on("click", onClickBtnStop);
-
-    player.src = SONGS[1].ruta;
 });
